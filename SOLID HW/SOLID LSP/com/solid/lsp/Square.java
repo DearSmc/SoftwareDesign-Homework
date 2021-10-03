1f26e99a81fcd1f65b29bf09727a6d5e510b6182
SUMINCHA CHALOWONG <63922836+DearSmc@users.noreply.github.com>
2021-10-03 21:52:06 +0700

    update Square.java

--- SOLID HW/SOLID LSP/com/solid/lsp/Square.java
+++ SOLID HW/SOLID LSP/com/solid/lsp/Square.java
@@ -2,12 +2,17 @@ package com.solid.lsp;
 
 public class Square extends Rectangle {
 
+    private int side;
     Square(int side) {
         this.setSide(side);
     }
 
     public void setSide(int side) {
+    	this.side = side;
         super.setWidth(side);
         super.setHeight(side);
+    }
+    public int getSide() {
+        return side;
     }
 }
